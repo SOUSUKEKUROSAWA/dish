6f788dbff8be3e95825549c69e9fbe01c62cc655
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2022-05-05 14:20:38 +0900

    Add:migration_file

---
+++ database/migrations/2022_05_04_132914_create_dishes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('dish_name', 30);
+            $table->integer('tag_id')->unsigned(); // tags_tableのidを参照する外部キー
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
