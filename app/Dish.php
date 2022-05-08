da0d2d097304501fe3e0ee89a9011b426153ceea
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2022-05-08 18:26:47 +0900

    Add:relation between Tag,Dish and Post

--- app/Dish.php
+++ app/Dish.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dish extends Model
 {
-    //
+    public function tag()
+    {
+        return $this->belongsTo('App\Tag');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
