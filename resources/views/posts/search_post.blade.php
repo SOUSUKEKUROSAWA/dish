9890d8d98e5a92f7c2d9dc921313a6f88882e5b5
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2022-07-04 03:40:09 +0900

    Fix:Comments in code

--- resources/views/posts/search_post.blade.php
+++ resources/views/posts/search_post.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1 class='title'>「{{ $dish->dish_name }}」の投稿一覧</h1>
     
-    <!--<script type="module" src="https://unpkg.com/x-frame-bypass"></script> <!-- x-frameを使ってサイトのプレビューを実装しようとしたが，うまく表示されなかった -->-->
+    <!--<script type="module" src="https://unpkg.com/x-frame-bypass"></script> <!-- x-frameを使ってサイトのプレビューを実装しようとしたが，うまく表示されなかった -->
     
     <!-- Reactのコンポーネントによってレンダリングする -->
     <div id='openpreview' data-posts='{{json_encode($posts)}}'></div> <!-- データ属性を利用し，postインスタンスをJSON形式に変換したものを，React側で識別できるようにする -->
